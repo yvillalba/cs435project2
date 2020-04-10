84737eb508e6fc57165f913b15f6c71a4320adb5
yvillalba <60801586+yvillalba@users.noreply.github.com>
2020-04-10 15:23:20 -0400

    Update question6.cpp

--- question6.cpp
+++ question6.cpp
@@ -12,7 +12,6 @@ using namespace std;
 struct GridNode {
   int data;
   unordered_set<GridNode*> neighbors;
-   //graphNode* next;
 };
 //********************************question 6a)*******************************
 class GridGraph {
@@ -45,9 +44,9 @@ class GridGraph {
    
     void addUndirectedEdge(GridNode* first,GridNode* second, int n){
       //chech if first and second are neighbors
-      cout<<"from "<<first->data<<" to "<<second->data<<"\n";
+    
       if (isNeighbor(first->data,second->data,n)){
-        cout<<"yes\n";
+        cout<<"from "<<first->data<<":("<<(first->data%n)<<","<<(first->data)/n<<") to " <<second->data<<":("<<(second->data%n)<<","<<(second->data)/n<<") and vice versa\n";
          // Add an edge from first to second. A new element is inserted to the neighbors of first. 
         first->neighbors.insert(second); 
         // Since graph is undirected, add an edge from second to first also 
@@ -100,30 +99,103 @@ GridGraph createRandomGridGraph(int n){
     }
     return graph1;
 }
+int manhattanDist(GridNode* current,GridNode* dstNode,int n){
+    int cx=(current->data)%n;//current square
+    int cy=(current->data)/n;
+    int gx=(dstNode->data)%n;//goal suare
+    int gy=(dstNode->data)/n;
+    int h = abs(cx-gx) + abs(cy-gy);
+    return h;
+}
+
+ // the node with the smallest g+h value not yet finalized.
+GridNode* minDist(unordered_map<GridNode*, pair<int, int>>& distances, unordered_set<GridNode*>& visited) {
+  GridNode* ans = nullptr;
+  int m = INT_MAX;
+  for (auto p : distances) 
+    if (!visited.count(p.first) && (p.second.first+p.second.second) <= m) {
+      m = p.second.first+p.second.second;
+      ans = p.first;
+    }
+  return ans;
+}
+void generatePath(unordered_map<GridNode*,GridNode*> parent,vector<GridNode*> &path,GridNode* sourceNode,GridNode* curr){
+  if (curr==sourceNode)
+     return;
+  else{
+     curr=parent[curr];
+     path.push_back(curr);
+     generatePath(parent,path,sourceNode,curr);
+
+  }
+
+}
 //********************************question 6d)*******************************
-//unordered_map<GridNode*, pair<int, int>> distances;
-vector<GridNode*> astar(GridNode* sourceNode,GridNode* dstNode){
-   vector<GridNode*> listNodes;
-   //1.Create an empty map of nodes to pairs of distances (g, h). g is the distance so far, and his the estimated distance to the goal using the heuristic. Initialize every node to map to infinity g
+
+vector<GridNode*> astar(GridNode* sourceNode,GridNode* dstNode,int n){
+   vector<GridNode*> path;
+
+   unordered_set<GridNode*> visited;
+   // stores predecessor of a vertex 
+   unordered_map<GridNode*,GridNode*> parent;
    
-   //2.Calculate the heuristic value for the origin h and set the distance for the origin to (0, h). Let curr be the origin.
-   
+   int h=0;
+   //1.Create an empty map of nodes to pairs of distances (g, h). g is the distance so far, and h is the estimated distance to the goal using the heuristic. 
+   unordered_map<GridNode*, pair<int, int>> distances;
+   //2.Calculate the heuristic value for the origin h  
+   h=manhattanDist(sourceNode,dstNode,n);
+   //set the distance for the origin to (0, h).
+   distances.insert(make_pair(sourceNode, make_pair(0,h))); 
+   // Let curr be the origin.
+    GridNode* curr=sourceNode;
    //3.While curr is not the destination node:
+    while(curr!=NULL){
         //a.“Finalize” curr.
+        visited.insert(curr);
+       
         //b.Iterate over its neighbors, “relax” each neighbor:
-            //c.For each neighbor that is not finalized, update its distance (if less than the current g value) to the sum of curr’s distance and the weight of the edge between curr and this neighbor.  Calculate the heuristic value for this neighbor and assign it to h.
-        //d.Set curr to the next min node – the node with the smallest g+h value not yet finalized.
-    //4.Return the g value for the destination node
-   return listNodes; 
+        for (auto neighbor :curr->neighbors){
+            
+            //if the node is not present in the hashmap, its distance will be infinity
+            if ( distances.find(neighbor) == distances.end() )  {
+                    distances.insert(make_pair(neighbor, make_pair(INT_MAX,0)));
+                    parent.insert(make_pair(neighbor, nullptr));   }
+            //c.For each neighbor that is not finalized, update its distance (if less than the current g value) to the sum of curr’s distance and the weight of the edge between curr and this neighbor.                
+            if  (!visited.count(neighbor)&&(distances[curr].first+1)<distances[neighbor].first){
+                   distances[neighbor].first = distances[curr].first + 1;
+                   //Calculate the heuristic value for this neighbor
+                   h=manhattanDist(neighbor,dstNode,n); 
+                   //and assign it to h.
+                   distances[neighbor].second = h;
+                   parent[neighbor] = curr;
+                }          
+        }
+        //d.Set curr to the next min node.
+          curr=minDist(distances,visited);
+    }
+    //4.Return path from source to destination 
+    if ( parent.find(dstNode) == parent.end() )  
+       cout<<"\nThere is not path between "<< sourceNode->data<<" and "<<dstNode->data;
+    else{
+        path.push_back(dstNode);
+        generatePath(parent,path,sourceNode,dstNode);
+     }
+      
+    
+   return path; 
 }
 int main(){
-   int n = 3; 
+   int n = 3; //node i is (i%n,i/n)->node 0 is (0,0), node 1 is (1,0), node 2 is (2,0) 
+   cout<<"Undirected graph"<<endl;
    GridGraph graph1=createRandomGridGraph(n);
-   GridNode* souceNode=graph1.getNode(0);//node zero is (0,0)
-   GridNode* destNode=graph1.getNode(99);//node 99 is (99,99)
-   vector<GridNode*> listNodes;
-   listNodes=astar(souceNode,destNode);
-   
-	
+   GridNode* sourceNode=graph1.getNode(0);//node zero is (0,0)
+   GridNode* destNode=graph1.getNode(n*n-1);//node 99 is (99,99)
+   vector<GridNode*> path;
+   cout<<"\nPath from "<<sourceNode->data<<":("<<(sourceNode->data%n)<<","<<(sourceNode->data)/n<<") to " <<destNode->data<<":("<<(destNode->data%n)<<","<<(destNode->data)/n<<")"<<endl;
+
+   path=astar(sourceNode,destNode,n);
+   for (auto p : path) {
+          cout<<p->data<<"("<<(p->data)%n<<","<<(p->data)/n<<")->";
+      } 
    return(0);
 }
